c2996f5afc90a34c014a17a81d0540097a3a896e
vinne25 <vinicius.silva.leite@hotmail.com>
2024-09-26 21:56:49 -0300

    Adicionando rotas e verbos

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ const sequelize = require('./config/config');
 const app = express();
 const userRoutes = require('./routes/userRoutes');
 const authRoutes = require('./routes/authRoutes');
+const setupSwagger = require('./swagger/swagger')
 
 app.use(express.json());
+setupSwagger(app)
 
 app.use('/api', userRoutes);
 app.use('/api', authRoutes);
--- controllers/userController.js
+++ controllers/userController.js
@@ -4,13 +4,47 @@ exports.listUsers = async (req, res) =>{
     try{
         const users = await User.findAll(
             {
-                attributes: ['username','loginuser','active'],
+                attributes: ['id','username','loginuser','active'],
             });
             if (users.length ===0){
                 return res.status(404).json({message:'Users not found'})
             }
             res.json(users);
     }catch(error) {
+        res.status(500).send(error.message);
+    }
+};
+
+exports.activeUsers = async(req, res) => {
+    try {
+        const {id} = req.params;
+        const user = await User.findOne({where: {id: id}});
+        
+        if (user){
+            user.active = true;
+            await user.save();
+            res.status(200).json({message: 'User activated successfuly'});
+        } else{
+            res.status(400).send('User not found')
+        }
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+};
+
+exports.deactiveUsers = async(req, res) => {
+    try {
+        const {id} = req.params;
+        const user = await User.findOne({where: {id: id}});
+        
+        if (user){
+            user.active = false;
+            await user.save();
+            res.status(200).json({message: 'User deactivated successfuly'});
+        } else{
+            res.status(400).send('User not found')
+        }
+    } catch (error) {
         res.status(500).send(error.message);
     }
 };
