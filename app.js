5f697b8da959e462bf6b17a20ab01d3f8ed13c3d
vinne25 <vinicius.silva.leite@hotmail.com>
2024-10-17 19:10:48 -0300

    setup routes

--- app.js
+++ app.js
@@ -3,13 +3,15 @@ const sequelize = require('./config/config');
 const app = express();
 const userRoutes = require('./routes/userRoutes');
 const authRoutes = require('./routes/authRoutes');
-const setupSwagger = require('./swagger/swagger')
+const setupSwagger = require('./swagger/swagger');
+const productRoutes = require('./routes/productRoutes')
 
 app.use(express.json());
 setupSwagger(app)
 
 app.use('/api', userRoutes);
 app.use('/api', authRoutes);
+app.use('/api', productRoutes);
 
 sequelize.sync()
         .then(
--- controllers/productController.js
+++ controllers/productController.js
@@ -3,7 +3,7 @@ const Product = require("../models/products");
 exports.getAllProducts = async (req, res) => {
   try {
     const products = await Product.findAll({
-      attributes: ["id", "name", "price", "description"],
+      attributes: ["name", "price", "description"],
     });
     if (products.length === 0) {
       return res.status(404).json({ message: "Product not found" });
@@ -19,11 +19,10 @@ exports.createProduct = async (req, res) => {
     const { name, price, description } = req.body;
     const existingProduct = await Product.findOne({ where: { name } });
     if (existingProduct) {
-      return res.status(200).json({ message: "Product already exists" });
+      return res.status(409).json({ message: "Product already exists" });
     }
     const product = await Product.create({ name, price, description });
-    res.status(201).json({ message: "Product created sucessfully", product });
-    //return res.status(200).json({message: 'ok register'});
+    res.status(201).json({ message: "Product created successfully", product });
   } catch (error) {
     res.status(500).send(error.message);
   }
@@ -38,16 +37,25 @@ exports.updateProduct = async (req, res) => {
       product.description = description;
       product.price = price;
       await product.save();
-      res.status(200).json({ message: "user updated successfuly!" });
+      res.status(200).json({ message: "Product updated successfully!" });
     } else {
-      res.status(404).json.send("User not found");
+      res.status(404).json({ message: "Product not found" });
     }
-  } catch {
+  } catch (error) {
     res.status(500).send(error.message);
   }
 };
 
 exports.deleteProduct = async (req, res) => {
   try {
-  } catch (error) {}
+    const { id } = req.params;
+    const product = await Product.findOne({ where: { id: id } });
+    if (!product) {
+      return res.status(404).json({ message: "Product not found" });
+    }
+    await product.destroy();
+    res.status(200).json({ message: "Product deleted successfully" });
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
 };
